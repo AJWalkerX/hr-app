4daf1a79041dee75a58c839c29380e7cc2c9cfbb
EmineKarabolat <emine.karabolat.01@gmail.com>
2024-12-26 18:05:22 +0300

    USERSERVICE ERROR EKLENDİ

--- src/main/java/com/ajwalker/exception/ErrorType.java
+++ src/main/java/com/ajwalker/exception/ErrorType.java
@@ -21,7 +21,8 @@ public enum ErrorType {
 	NOTFOUND_COMPANY(3003,"Şirket bulunamadı",HttpStatus.BAD_REQUEST),
 	ALREADY_EXIST_COMPANY(3003,"Şirket Siteme kayitli! Kayit olmadan once sirketiniz ile iletisime geciniz!",HttpStatus.BAD_REQUEST),
 	NOTFOUND_MEMBERSHIP_PLAN(2003,"Üyelik planı bulunamadı",HttpStatus.BAD_REQUEST),
-	NOTFOUND_MANAGER(5003,"Manager bulunamadı!!!",HttpStatus.BAD_REQUEST);
+	NOTFOUND_MANAGER(5003,"Manager bulunamadı!!!",HttpStatus.BAD_REQUEST),
+	NOTFOUND_WORKHOLIDAY_INPENDING(7003,"Herhangi bir izin isteği bulunamadı!!!",HttpStatus.BAD_REQUEST);
 	
 	
 	int code;
