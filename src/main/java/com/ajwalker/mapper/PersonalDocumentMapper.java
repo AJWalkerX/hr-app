2cce28ce30a38ca2896d54c8bd944c3759235c2b
Mehtor99 <mehtor99@gmail.com>
2024-12-27 19:05:32 +0300

    ManagerController'a update methodu yazıldı

---
+++ src/main/java/com/ajwalker/mapper/PersonalDocumentMapper.java
@@ -0,0 +1,16 @@
+package com.ajwalker.mapper;
+
+import com.ajwalker.dto.request.IUpdateEmployeeRequestDto;
+import com.ajwalker.entity.PersonalDocument;
+import com.ajwalker.entity.User;
+import org.mapstruct.*;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "string", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface PersonalDocumentMapper {
+	
+	PersonalDocumentMapper INSTANCE = Mappers.getMapper(PersonalDocumentMapper.class);
+	
+	@BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+	PersonalDocument fromIUpdateEmployeeRequestDto(final IUpdateEmployeeRequestDto dto, @MappingTarget PersonalDocument personalDocument);
+}
--- src/main/java/com/ajwalker/mapper/UserMapper.java
+++ src/main/java/com/ajwalker/mapper/UserMapper.java
@@ -1,10 +1,9 @@
 package com.ajwalker.mapper;
 
+import com.ajwalker.dto.request.IUpdateEmployeeRequestDto;
 import com.ajwalker.dto.request.RegisterRequestDto;
 import com.ajwalker.entity.User;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.ReportingPolicy;
+import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "string", unmappedTargetPolicy = ReportingPolicy.IGNORE)
@@ -12,4 +11,7 @@ public interface UserMapper {
 	UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 	
 	User fromRegisterDto(RegisterRequestDto dto); //sağdakine yazdıklarımızla başa yazdığımızın içini dolduruyor.
+	
+	@BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+	User fromIUpdateEmployeeRequestDto(final IUpdateEmployeeRequestDto dto,@MappingTarget User user);
 }
