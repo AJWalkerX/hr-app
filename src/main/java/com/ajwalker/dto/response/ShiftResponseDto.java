d0defee90f386d01e9bd67af135c76080a79808a
Alexander Joseph Walker <a.j.walker99@outlook.com>
2025-01-06 15:57:26 +0000

    ShiftResponseDto updated

--- src/main/java/com/ajwalker/dto/response/ShiftResponseDto.java
+++ src/main/java/com/ajwalker/dto/response/ShiftResponseDto.java
@@ -7,7 +7,7 @@ import java.time.LocalTime;
 
 public record ShiftResponseDto(
         Long shiftId,
-        String ShiftName,
+        String shiftName,
         LocalTime beginTime,
         LocalTime endTime
 ) {
